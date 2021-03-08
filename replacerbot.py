5b42616ac8aa081c74a77f8262ed9f61c9b4ea8d
Mitchell Greene <mitchell.lee.greene@gmail.com>
2021-03-08 18:47:59 -0500

    add a check for the env file and to create one if none is found

--- replacerbot.py
+++ replacerbot.py
@@ -3,6 +3,16 @@ import os, discord
 
 from dotenv import load_dotenv
 
+print("-----------Looking for env file------------")
+try:
+    with open(r"token.env", "r") as env_file:
+        print("Found env file.")      
+except IOError:
+    print("File named 'token.env' not found. Creating file...please fill out with correct info")
+    with open(r"token.env", "w") as env_file:
+        env_file.write("#token.env\nDISCORD_TOKEN=")
+    exit()
+
 load_dotenv('token.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 
