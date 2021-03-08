8c7a466850e6afe2a8642f93467cca42e6712fdd
Mitchell Greene <mitchell.lee.greene@gmail.com>
2021-03-08 18:24:53 -0500

    finished the client connection script, add env var file

---
+++ replacerbot.py
@@ -0,0 +1,16 @@
+#replacerbot.py
+import os, discord
+
+from dotenv import load_dotenv
+
+load_dotenv('token.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
