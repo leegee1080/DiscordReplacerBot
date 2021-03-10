30b9f3f572319cc04adf8dc10ddd57f191759e34
Mitchell Greene <mitchell.lee.greene@gmail.com>
2021-03-09 20:56:16 -0500

    added a message for: startup, shutdown, and response to word'ah', added a env var for guilds

--- replacerbot.py
+++ replacerbot.py
@@ -1,6 +1,7 @@
 #replacerbot.py
 import os, discord
 
+from discord.ext import commands
 from dotenv import load_dotenv
 
 print("-----------Looking for env file------------")
@@ -10,20 +11,78 @@ try:
 except IOError:
     print("File named 'token.env' not found. Creating file...please fill out with correct info")
     with open(r"token.env", "w") as env_file:
-        env_file.write("#token.env\nDISCORD_TOKEN=")
+        env_file.write("#token.env\nDISCORD_TOKEN=\nDISCORD_GUILD=")
     exit()
 
 load_dotenv('token.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
+print("Connecting to discord...\n")
 
-client = discord.Client()
+#using a decorator
+# client = discord.Client()
+#  
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+#     #diff methods for finding guild membership
+#     guild = discord.utils.get(client.guilds, name=GUILD)
 
+#     # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+#     # for guild in client.guilds:
+#     #     if guild.name == GUILD:
+#     #         break
+    
+#     print(f'{client.user} is connected to the following guild:\n{guild.name}(id: {guild.id})\n')
+
+#     members = '\n - '.join([member.name for member in guild.members])
+#     print(f'Guild Members:\n - {members}')
+#     return
+
+#using a subclass
+class QuirtisBotClient(discord.Client):
+    async def on_ready(self):
+        print(f'{client.user} has connected to Discord!')
+        
+        #diff methods for finding guild membership
+        guild = discord.utils.get(client.guilds, name=GUILD)
+
+        # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+        # for guild in client.guilds:
+        #     if guild.name == GUILD:
+        #         break
+        
+        print(f'{client.user} is connected to the following guild:\n{guild.name}(id: {guild.id})\n')
+
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
+
+        #two methods for finding channels
+        channel = discord.utils.get(guild.channels, name='general')
+        # channel = client.get_channel(818616194931228686)
+        
+        await channel.send("I'm here!")
+    
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+        
+        if message.content == "ah!":
+            response = "ooh!"
+            await message.channel.send(response)
+
+        if message.content == "kill!":
+            await message.channel.send("I'm dead.")
+            await client.close()
+        
+
+client = QuirtisBotClient()
 client.run(TOKEN)
 
-#test working
+
 #https://realpython.com/how-to-make-a-discord-bot-python/
+#https://discordpy.readthedocs.io/en/latest/api.html#
