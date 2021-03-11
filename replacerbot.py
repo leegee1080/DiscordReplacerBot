9a209ae91a4ce4c1ebeefc5a8374ade38f5102a1
leegee1080 <mitchell.lee.greene@gmail.com>
2021-03-11 15:17:46 -0500

    rearranged the functions for all bot commands, documented the code better

--- replacerbot.py
+++ replacerbot.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python3
 #replacerbot.py
-import os, discord, random, re
 
+#imports
+import os, discord, random, re
 from discord.ext import commands
 from dotenv import load_dotenv
 
+#global vars
+rand_num_top = 1
+adj_list = []
+is_sleeping = False
+
+##commands
+#!QB ----> keyword
+re_keyword = re.compile(r"\b!QB\b")
+
+#"QB add <word> --->adds a word to the adj list
+#"QB remove <word>" ---> removes a word to the adj list
+#"!QB sleep" --->sleeps the bot.
+#"!QB awake" --->awakens the bot.
+#"!QB calm" ---> reduces the number of times the bot checks messages
+re_command_check = re.compile(r"\b!QB\s(add|remove|calm|sleep|awake)\s([a-z][A-Z]*)\b")
+command_dict_switch = {
+    "add": word_adder,
+    "remove": word_remover,
+    "calm": bot_calmer,
+    "sleep": bot_sleeper,
+    "awake": bot_awaker
+}
 
 
+#"Kill QuirtisBot!" --->kills the bot..
+re_kill_check = re.compile(r"\bKill QuirtisBot!\b")
+
+
+#connection to discord
 print("-----------Looking for env file------------")
 try:
     with open(r"token.env", "r") as env_file:
@@ -20,7 +48,7 @@ except IOError:
 print("-----------Looking for adj file------------")
 try:
     with open (r"adjectives.txt") as adj_file:
-        lines = adj_file.readlines()
+        adj_list = adj_file.readlines()
         print("Found adj file.")
 except IOError:
     print("'adjectives.txt' not found.")
@@ -29,20 +57,70 @@ except IOError:
 load_dotenv('token.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
 print("Connecting to discord...\n")
 
-def adj_replacer(message):
-    print(f"starting replacer on message '{message}'")
-    output = ""
-    for adj in lines:
+#functions
+def message_checker(message):
+    if re.search(re_keyword, message.content):
+        command = re.search(re_command_check, message.content)
+        command_dict_switch[command.group(1)](message, command.group(2))
+        return
+    if(random.randint(1, rand_num_top) == 1):
+        print("I'm looking at the message!")
+        new_message = adj_replacer(message.content)
+        if(new_message == ""):
+            return
+        else:
+            await message.channel.send(new_message)
+            return
+    return
+
+def adj_replacer(message_content):
+    print(f"starting replacer on message '{message_content}'")
+    adjrep_output = ""
+    for adj in adj_list:
         regex_adj = adj.rstrip("\n")
         regex_adj = regex_adj
         adj_check = re.compile(r"\b" + regex_adj + r"\b", re.IGNORECASE)
-        if(adj_check.search(message)):
-            output = f"You're {regex_adj}."
+        if(adj_check.search(message_content)):
+            adjrep_output = f"You're {regex_adj}."
             break
-    return output
+    return adjrep_output
+
+def bot_awaker(first_param, orignal_message):
+    is_sleeping = False
+    return print("I'm awake.")
+
+def bot_sleeper(first_param, orignal_message):
+    is_sleeping = True
+    return print("I'm asleep.")
+
+def bot_calmer(first_param, orignal_message):
+    rand_num_top = 3
+    await orignal_message.channel.send("No you.")
+    return print("I'm calm.")
+
+def word_adder(word_to_add, orignal_message):
+    wordadd = word_to_add
+    adj_list.append(wordadd)
+    with open (r"adjectives.txt", "w") as adj_file:
+        adj_file.writelines(adj_list)
+    await orignal_message.channel.send(f"Sure, I guess {word_to_add} is a word.")
+    return print(f"'{wordadd}' added")
+
+def word_remover(word_to_remove, orignal_message):
+    wordrem = word_to_remove
+    temp_list = []
+    if wordrem in adj_list:
+        for adj in adj_list:
+            if adj != wordrem:
+                temp_list.append(adj)
+        with open (r"adjectives.txt", "w") as adj_file:
+            adj_file.writelines(temp_list)
+            adj_list = temp_list
+        await orignal_message.channel.send(f"Wow, I guess {word_to_remove} isn't a word.")
+        return print(f"'{wordrem}' removed")
+    return print(f"'{wordrem}' is not in the list")
 
 ###using a decorator
 # client = discord.Client()
@@ -68,7 +146,6 @@ def adj_replacer(message):
 
 ###using a subclass
 class QuirtisBotClient(discord.Client):
-    rand_num_top = 1
 
     async def on_ready(self):
         print(f'{client.user} has connected to Discord!')
@@ -95,25 +172,17 @@ class QuirtisBotClient(discord.Client):
         await channel.send("I'm here!")
     
     async def on_message(self, message):
+        #check to make sure the bot isnt' responding to itself.
         if message.author == client.user:
             return
 
+        #a statment to kill the bot for good.
         if message.content == "kill QuirtisBot!":
             await message.channel.send("I'm dead.")
             await client.close()
 
-        if message.content == "calm down QuirtisBot!":
-            self.rand_num_top = 3
-            await message.channel.send("No you.")
-            return
-
-        if(random.randint(1,self.rand_num_top) == 1):
-            print("I'm looking at the message!")
-            new_message = adj_replacer(message.content)
-            if(new_message == ""):
-                return
-            else:
-                await message.channel.send(new_message)
+        #check the message.content for certain phrases
+        message_checker(message)
 
         
 
